5a44fb42ec2cd571d7febbd5cc830478bf7dc358
Frank Mayer <frank@frankmayer.net>
2013-11-17 12:56:31 +0200

    Added NOTES.md file in performance-tests directory including some notes on the performance findings. Removed the notes from the tests themselves.

--- tests/performance/InstantiateHttpRequest02DirectlyTest.php
+++ tests/performance/InstantiateHttpRequest02DirectlyTest.php
@@ -17,19 +17,6 @@ use frankmayer\ArangoDbPhpCore\Connectors\Http\CurlHttpConnector;
 use frankmayer\ArangoDbPhpCore\Connectors\Http\HttpRequest;
 
 
-/*
- * These Notes are for test that ran on PHP 5.4.x
- * Notes: The instantiation of the HttpRequest object is about the same between direct naming and through resolving Client::requestClass
- * The small difference is due to PHP internal optimization, but whichever of both gets tested first is a little slower than the second.
- * Even when swapping the tests.
- *
- * By instantiating through the IOC container, the time doubles, but it shouldn't have considerable impact on overall performance.
- * If someone needs to squeeze a bit more out of it, then the IOC container can be avoided and the instantiation can be made directly or via
- * resolving the Client:requestClass
- *
- */
-
-
 class InstantiateHttpRequestDirectlyTest extends
     \PHPUnit_Framework_TestCase
 {
