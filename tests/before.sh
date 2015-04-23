d6e3bc7a38339fb68ffe4af82158b50b735e5f91
Frank Mayer <frank@frankmayer.net>
2015-04-23 17:44:21 +0300

    no need to have the latest composer version, for now...

--- tests/before.sh
+++ tests/before.sh
@@ -3,8 +3,8 @@
 # This script is run by Travis CI before the test script.
 # It updates composer
 
-echo -- Updating Composer
-wget http://getcomposer.org/composer.phar
+#echo -- Updating Composer
+#wget http://getcomposer.org/composer.phar
 php composer.phar require satooshi/php-coveralls:dev-master --dev --no-progress --prefer-source
 
 # composer self-update
