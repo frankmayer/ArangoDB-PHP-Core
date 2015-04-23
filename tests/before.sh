d20b1bbd5bd0ca8ab07c16d73ad8a385e7a2e7fa
Frank Mayer <frank@frankmayer.net>
2015-04-23 08:12:25 +0300

    A few more canges...

--- tests/before.sh
+++ tests/before.sh
@@ -5,7 +5,7 @@
 
 echo -- Updating Composer
 wget http://getcomposer.org/composer.phar
-# php composer.phar require satooshi/php-coveralls:dev-master --dev --no-progress --prefer-source
+php composer.phar require satooshi/php-coveralls:dev-master --dev --no-progress --prefer-source
 
 # composer self-update
 
