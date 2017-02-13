c5f7837b976f7e2e38579104515edfe602e5dece
frank <frank@frankmayer.net>
2017-02-14 01:54:11 +0200

    Try to enable xdebug in HHVM...

--- tests/before.sh
+++ tests/before.sh
@@ -2,14 +2,3 @@
 #
 # This script is run by Travis CI before the test script.
 # It updates composer
-
-#echo -- Updating Composer
-#wget http://getcomposer.org/composer.phar
-composer require satooshi/php-coveralls:dev-master --dev --no-progress --prefer-source --ignore-platform-reqs
-
-# composer self-update
-
-# those are not needed
-#
-# echo -- Installing the dependencies
-# composer install --dev
